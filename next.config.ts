c01a2066a208e677da42f27aece18e065236f5e0
Shand Ivan <ivansinohon28@gmail.com>
2026-05-25 11:32:02 +0800

    add DNS server configuration and update Next.js config for allowed origins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  allowedDevOrigins: ["10.3.2.100"], // Lab B
+}
 
-export default nextConfig;
+export default nextConfig
